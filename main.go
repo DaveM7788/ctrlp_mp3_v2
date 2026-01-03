75b30b0aa2b6a3b18e66f6ed83fe16c5b32a61b6
DaveM7788 <david.mcdermott98@gmail.com>
2026-01-02 22:12:37 -0600

    Collect audio files
    and layout general structure

--- main.go
+++ main.go
@@ -6,11 +6,29 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"slices"
 	"strings"
 	"time"
 
 	"go.senan.xyz/taglib"
 )
+
+type AudioFile struct {
+	filePath     string
+	songName     string
+	artist       string
+	album        string
+	albumArtPath string
+}
+
+type AudioAlbum struct {
+	album        string
+	albumArtPath string
+}
+
+var AudioFiles = []AudioFile{}
+var UniqueArtists = []string{}
+var UniqueAlbums = []AudioAlbum{}
 
 func main() {
 	start := time.Now()
@@ -22,6 +40,13 @@ func main() {
 		fmt.Println("No arguments passed. Need filepath for music directory. Exiting...")
 	}
 
+	collectAudioFiles(rootPath)
+
+	elapsed := time.Since(start)
+	fmt.Printf("Static site generation time: %s.\n", elapsed)
+}
+
+func collectAudioFiles(rootPath string) {
 	count := 0
 	err := filepath.WalkDir(rootPath, func(path string, d fs.DirEntry, err error) error {
 		if err != nil {
@@ -30,29 +55,45 @@ func main() {
 
 		if strings.HasSuffix(path, ".mp3") {
 			count += 1
+			songTitle := "Unknown"
 			artist := "Unknown"
 			album := "Unknown"
 
 			tags, _ := taglib.ReadTags(path)
 			//fmt.Printf("tags: %v\n", tags)
-			fmt.Printf("AlbumArtist: %q\n", tags[taglib.AlbumArtist])
-			artistSlice := tags[taglib.AlbumArtist]
+			//fmt.Printf("AlbumArtist: %q\n", tags[taglib.AlbumArtist])
 
+			songNameSlice := tags[taglib.Title]
+			if len(songNameSlice) != 0 {
+				songTitle = songNameSlice[0]
+			} else {
+				// take file name
+			}
+
+			artistSlice := tags[taglib.AlbumArtist]
 			if len(artistSlice) != 0 {
 				fmt.Println("AlbumArtist: " + artistSlice[0])
 				artist = artistSlice[0]
 			}
 
 			albumSlice := tags[taglib.Album]
-
 			if len(albumSlice) != 0 {
 				fmt.Println("Album: " + albumSlice[0])
 				album = albumSlice[0]
 			}
 
-			//fmt.Printf("Album: %q\n", tags[taglib.Album])
-			//albumSlice := tags[taglib.Album]
-			//fmt.Printf("TrackNumber: %q\n", tags[taglib.TrackNumber])
+			audioFile := AudioFile{
+				filePath:     path,
+				songName:     songTitle,
+				artist:       artist,
+				album:        album,
+				albumArtPath: "assets/images/controlp_sq_jpg.jpg",
+			}
+
+			albumInfo := AudioAlbum{
+				album:        album,
+				albumArtPath: "assets/images/controlp_sq_jpg.jpg",
+			}
 
 			embeddedImg, _ := taglib.ReadImage(path)
 			if len(embeddedImg) != 0 && artist != "Unknown" && album != "Unknown" {
@@ -66,17 +107,48 @@ func main() {
 				if err != nil {
 					fmt.Printf("Error writing embedded album image: %v\n", err)
 				}
+				audioFile.albumArtPath = filePath
+				albumInfo.albumArtPath = filePath
+			}
+
+			AudioFiles = append(AudioFiles, audioFile)
+			fmt.Println("Added the following song name " + AudioFiles[0].songName)
+
+			if !slices.Contains(UniqueArtists, artist) {
+				UniqueArtists = append(UniqueArtists, artist)
+			}
+
+			for i := range UniqueAlbums {
+				if UniqueAlbums[i].album == album {
+					continue
+				}
+				UniqueAlbums = append(UniqueAlbums, albumInfo)
 			}
 		}
+
+		generateAllAlbumsPage()
+		generateAllArtistsPage()
+
 		return nil
 	})
 
 	if err != nil {
 		log.Fatalf("Error walking the directory: %v", err)
 	}
+	fmt.Printf("Found %f audio files.\n", count)
+}
 
-	elapsed := time.Since(start)
-	fmt.Printf("directory audio checks took %s.\n", elapsed)
+func generateAllAlbumsPage() {
+	for i := range UniqueAlbums {
+		albumName := UniqueAlbums[i]
+		fmt.Println("album name" + albumName.album)
+	}
+}
 
-	fmt.Printf("count was %f.\n", count)
+func generateAllArtistsPage() {
+
+}
+
+func generateAllSongsPage() {
+
 }
